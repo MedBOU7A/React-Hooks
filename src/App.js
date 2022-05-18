1ce16916efc16c80f98e506135bc279b4f7320bd
Med BOU7A <bou7amed92@gmail.com>
2022-05-18 16:33:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
+import MovieList from './components/MovieList';
+import { moviesData } from './data'
+import Search from './components/Search';
+import AddMovie from './components/AddMovie'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-function App() {
+import About from './components/About';
+
+
+
+
+const App = () => {
+  const [movies, setMovies] = useState(moviesData);
+  const [searchRating, setSearchRating] = useState(1);
+  const [searchValue, setSearchValue] = useState("") ;
+
+  const handelAdd =(newMovie)=>  setMovies([...movies, newMovie]);
+  const handelRating =(newRaing)=> setSearchRating(newRaing);
+  const handelSearch =(e)=> setSearchValue(e.target.value)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className='App' >
+   <Search searchRating={searchRating} searchValue={searchValue} 
+    handelRating={handelRating} handelSearch={handelSearch} />
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<MovieList movies={movies} searchRating={searchRating} searchValue={searchValue} />} />
+      <Route path='/film/:id' element={<About movies={movies} />}/>
+    </Routes>
+   </BrowserRouter>
+   <AddMovie handelAdd={handelAdd} />
+  </div>
+
+  )
 }
 
 export default App;
+
